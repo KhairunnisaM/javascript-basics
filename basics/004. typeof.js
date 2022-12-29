f9b4ebbe871d5135d46bdaaf2c0256a79a770ca9
Khairunnisa <khair.moghal@gmail.com>
2022-12-30 00:04:52 +0530

    operators and type of

---
+++ basics/004. typeof.js
@@ -0,0 +1,7 @@
+typeof 9 //number
+typeof "Khairunnisa" //string
+typeof (10>8) //boolean
+typeof 10>8 //false
+//typeof 12 56 //error
+typeof 12 + 56 //number56
+
---
+++ basics/005. arthimatic.js
@@ -0,0 +1,12 @@
+5+3 //8
+5-3 //2
+5*3 //15
+5/3 //1.66
+5%3 //2 it is a value of reminder
+5++
+'5'+'4' // '54'
+"Khairunnisa" + " Begam"
+"Khairunnisa"+56
+"6"-"7"
+"6"- "M"
+"M"*6
